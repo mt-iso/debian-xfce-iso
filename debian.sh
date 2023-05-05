f76e4d188a9c243a88e5eac255c99228e97e9bb4
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-05-05 14:19:58 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -88,6 +88,12 @@ EOF
 
 #### firmware
 chroot chroot apt-get install firmware-linux firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree firmware-amd-graphics firmware-realtek bluez-firmware -yq
+# https://github.com/Whonix/whonix-legacy/blob/master/debian/whonix-legacy.preinst#L49
+#echo b43-fwcutter b43-fwcutter/cut_firmware boolean true | debconf-set-selections
+#echo firmware-ipw2x00 firmware-ipw2x00/license/accepted boolean true | debconf-set-selections
+#echo firmware-iwlwifi firmware-iwlwifi/license/accepted boolean true | debconf-set-selections
+#echo firmware-ralink firmware-ralink/license/accepted boolean true | debconf-set-selections
+
 
 
 #### stock kernel 
